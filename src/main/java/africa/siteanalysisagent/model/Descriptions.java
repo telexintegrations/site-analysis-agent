af71b0d5d9a4e900401f94d707920e50ce92820b
Starkthegreat <EmmanuelKipropKimutai@gmail.com>
2025-03-12 19:52:26 +0300

    feat: add telex configuration api

---
+++ src/main/java/africa/siteanalysisagent/model/Descriptions.java
@@ -0,0 +1,10 @@
+package africa.siteanalysisagent.model;
+
+public record Descriptions(
+        String app_name,
+        String app_description,
+        String app_logo,
+        String app_url,
+        String background_color) {
+
+}
