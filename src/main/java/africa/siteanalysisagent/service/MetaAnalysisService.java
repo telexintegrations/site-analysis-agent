5c85c7f33171003a47763466d071bf090f2f5bbe
gabeman <gabrielayochukwu@gmail.com>
2025-03-12 03:53:27 +0100

    feat: create MetaAnalysisService interface for site analysis

--- src/main/java/africa/siteanalysisagent/service/MetaAnalysisService.java
+++ src/main/java/africa/siteanalysisagent/service/MetaAnalysisService.java
@@ -1,4 +1,16 @@
 package africa.siteanalysisagent.service;
 
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface MetaAnalysisService {
+    boolean isSingleUrl (String url);
+
+    Document scrape(String url) throws IOException;
+
+    List<String> checkMetaTags(Document document);
+
+    String generateSeoReport(String url);
 }
