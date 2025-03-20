827642bf0de88fe16760374ee1c28a9843a29d3e
Noxxspring <gabrielayochukwu@gmail.com>
2025-03-20 15:01:12 +0100

    feat: added new feat DTOs for better structure and usability

--- src/main/java/africa/siteanalysisagent/dto/Button.java
+++ src/main/java/africa/siteanalysisagent/dto/Button.java
@@ -1,2 +1,27 @@
-package africa.siteanalysisagent.dto;public class Button {
+package africa.siteanalysisagent.dto;
+
+public class Button {
+    private String text;
+    private String value;
+
+    public Button(String text, String value){
+        this.text = text;
+        this.value = value;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/africa/siteanalysisagent/dto/TelexEvent.java
+++ src/main/java/africa/siteanalysisagent/dto/TelexEvent.java
@@ -1,2 +1,60 @@
-package africa.siteanalysisagent.dto;public class TelexEvent {
+package africa.siteanalysisagent.dto;
+
+public class TelexEvent {
+    private String type; // Event type (e.g., "message", "interactive_message")
+    private String userId; // ID of the user who triggered the event
+    private String text; // Message text or button value
+    private String action; // Action ID (for button clicks)
+    private String webhookUrl; // Channel or DM ID where the event occurred
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getWebhookUrl() {
+        return webhookUrl;
+    }
+
+    public void setWebhookUrl(String webhookUrl) {
+        this.webhookUrl = webhookUrl;
+    }
+
+    @Override
+    public String toString(){
+        return "TelexEvent{" +
+                "type='" + type + '\'' +
+                ", userId='" + userId + '\'' +
+                ", text='" + text + '\'' +
+                ", action='" + action + '\'' +
+                ", channelId='" + webhookUrl + '\'' +
+                '}';
+    }
 }
